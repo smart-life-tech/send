88e04915f584edbbd64fda0a3c2fc0a8c7795a3d
smart-life-tech <christlightworld@gmail.com>
2023-01-27 11:39:20 +0100

    modified:   package-lock.json

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React, { useState } from 'react';
-import { Solana } from '@solana/web3.js';
+import { Connection } from '@solana/web3.js';
 import WalletConnect from '@walletconnect/browser';
+const { struct, u32, ns64 } = require("@solana/buffer-layout");
+const { Buffer } = require("buffer");
+const web3 = require("@solana/web3.js");
+let keypair = web3.Keypair.generate();
+let payer = web3.Keypair.generate();
 
 function StakeForm() {
   const [amount, setAmount] = useState(0);
   const [stakeError, setStakeError] = useState(null);
   const [walletConnected, setWalletConnected] = useState(false);
-  const solana = new Solana('https://testnet.solana.com');
+  const solana = new Connection('https://testnet.solana.com');
 
   async function handleSubmit(event) {
     event.preventDefault();
 
     try {
       // Connect to Solana
-      await solana.connect();
+      //await solana.connect();
+      let connection = new web3.Connection(web3.clusterApiUrl("testnet"));
+      await web3.sendAndConfirmTransaction(connection, allocateTransaction, [
+        payer,
+        keypair,
+      ]);
       // Connect to WalletConnect
       const walletConnector = new WalletConnect({
         bridge: 'https://bridge.walletconnect.org',
